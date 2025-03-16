d9a37e5eb2f7063c4bc3690be901811952a3354b
Mikhail_Nagibin <nagibimi@gmail.com>
2025-03-16 13:32:32 +0300

    add some requests

--- our_requests.py
+++ our_requests.py
@@ -21,3 +21,26 @@ def get_all_tails(adres: str) -> set:
     while len(tails) != 16:
         tails.add(tuple(map(tuple, requests.get(adres).json()['message']['data'])))
     return tails
+
+
+def get_coords(adres: str) -> dict:
+    return requests.get(f'{adres}/coords').json()
+
+
+def insert_adres(conn: psycopg2.extensions.connection, adres: str):
+    cur = conn.cursor()
+    cur.execute("insert into tests(adres) values %s", (adres, ))
+    conn.commit()
+
+
+def get_adres_id(cur: psycopg2.extensions.cursor, adres: str):
+    cur.execute("select id from tests where adres = %s", (adres, ))
+    return cur.fetchall()
+
+
+def insert_coords(conn: psycopg2.extensions.connection, test_id, data: dict):
+    cur = conn.cursor()
+    cur.execute("insert into coords values %s",
+                (test_id, *data['listener'], *data['sender'], *data['price']))
+    conn.commit()
+
